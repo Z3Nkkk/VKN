d21d2be814001cce77fb0abbb4f8b1c16ebd9746
Z3Nkkk <113393618+Z3Nkkk@users.noreply.github.com>
2022-09-21 10:58:19 +0300

    Add files via upload

---
+++ lb5_1.py
@@ -0,0 +1,27 @@
+#Варіант 6 
+#1. Користувач задає трицифрове число. Написати програму, яка обчислить різницю першої і останньої цифр цього числа.
+import math as m
+
+x = float(input("Введіть x: "))
+ 
+def func_el1 ( x ):
+    a = -9 + m.log( x, m.e) + m.sqrt(x)
+    return(a)
+def func_el2 ( x ):
+    b = m.cos(x) + m.sin(2*x)
+    return(b)
+def func_el3 ( x ):
+    c = m.fabs( -m.pow( x, 2) + 2*m.pow( x, -x) )
+    return(c)
+
+if x >= 5 :
+    print ("Значення функції: ", round (func_el1(x), 2))
+elif x > -0.5 and x < 5 :
+    print ("Значення функції: ", round (func_el2(x), 2))
+else:
+    print ("Значення функції: ", round (func_el3(x), 2))
+    
+    
+    
+
+ 

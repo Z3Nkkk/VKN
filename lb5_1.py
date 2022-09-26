73dd5c2096f725480a09438ac3728e772ad163ef
Z3Nkkk <113393618+Z3Nkkk@users.noreply.github.com>
2022-09-26 10:28:13 +0300

    Update lb5_1.py

--- lb5_1.py
+++ lb5_1.py
@@ -4,19 +4,19 @@ import math as m
 
 x = float(input("Введіть x: "))
  
-def func_el1 ( x ):
-    a = -9 + m.log( x, m.e) + m.sqrt(x)
+def func_el1 ( x1 ):
+    a = -9 + m.log( x1, m.e) + m.sqrt(x1)
     return(a)
-def func_el2 ( x ):
-    b = m.cos(x) + m.sin(2*x)
+def func_el2 ( x2 ):
+    b = m.cos(x2) + m.sin(2*x2)
     return(b)
-def func_el3 ( x ):
-    c = m.fabs( -m.pow( x, 2) + 2*m.pow( x, -x) )
+def func_el3 ( x3 ):
+    c = m.fabs( -m.pow( x3, 2) + 2*m.pow( x3, -x3) )
     return(c)
 
 if x >= 5 :
     print ("Значення функції: ", round (func_el1(x), 2))
-elif x > -0.5 and x < 5 :
+elif x > -0.5 :
     print ("Значення функції: ", round (func_el2(x), 2))
 else:
     print ("Значення функції: ", round (func_el3(x), 2))
